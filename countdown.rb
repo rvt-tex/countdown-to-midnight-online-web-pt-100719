87c90c52004a1316048ef842bacc9c4a793ae98f
Ruel Tingling <rvtingling@yahoo.com>
2019-10-09 21:35:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+   num -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+#countdown(10)
+
+def countdown_with_sleep(num)
+  one_sec = sleep 5
+  while num > 0
+    puts "#{num}"
+    num -= one_sec
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+#countdown_with_sleep(12)
